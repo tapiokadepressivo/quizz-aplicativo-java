c201f086aca2f5e7c1d0a595bdf315fd6357d349
gabriel <107139885+tapiokadepressivo@users.noreply.github.com>
2023-10-10 14:25:44 -0300

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -1,8 +1,12 @@
-// creating an array and passing the number, questions, options, and answers
+// creating an array and passing the number, questions, options and answers
 let questions = [
   {
     numb: 1,
-    question: "Quem foi a unica pessoa a receber o Premio Nobel em áreas diferentes?",
+    question: "Quem foi a única pessoa na história a receber o Prêmio Nobel em áreas científicas diferentes?",
     answer: "Marie Curiee",
-    option: 
+    option: [
+      "Albert Einstein",
+      "Marie Curiee",
+      "Stephen Hawking",
+      "Linus Pauling"
       
