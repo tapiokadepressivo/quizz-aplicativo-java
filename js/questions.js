636c38d17894353b5f9e06ae1518d9e08af0b5c0
gabriel <107139885+tapiokadepressivo@users.noreply.github.com>
2023-10-24 13:02:58 -0300

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -1,12 +1,73 @@
-// creating an array and passing the number, questions, options and answers
+// creating an array and passing the number, questions, options, and answers
 let questions = [
   {
     numb: 1,
-    question: "Quem foi a única pessoa na história a receber o Prêmio Nobel em áreas científicas diferentes?",
-    answer: "Marie Curiee",
-    option: [
-      "Albert Einstein",
-      "Marie Curiee",
-      "Stephen Hawking",
-      "Linus Pauling"
+    question: "qual foi o clube que ganhou o mundial em 2012 ?",
+    answer: "Corinthians",
+    options: [
+      "Corinthians",
+      "Palmeiras",
+      "Chealse",
+      "Santos"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Quantos Mundiais tem o Corinthans ?",
+    answer: "2",
+    options: [
+      "3",
+      "2",
+      "1",
+      "0"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Quem foi o melhor zagueiro do Brasil de todos os tempos??",
+    answer: "Aldair Santos do Nascimento",
+    options: [
+      "NINO",
+      "Edson Arantes do Nascimento",
+      "Aldair Santos do Nascimento",
+      "Gustavo Gómez"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Quantos Paulistas tem o Corinthians?",
+    answer: "30",
+    options: [
+      "12",
+      "21",
+      "30",
+      "33"
+    ]
+  },
+  {
+    numb: 5,
+    question: "quantos gols tem roger guedes pelo corinthians?",
+    answer: "43",
+    options: [
+      "43",
+      "25",
+      "33",
+      "13"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
       
